9fa710830de499019d02d16908dd73efc3bf97ef
Timmy2324 <Salikhov.T.S@yandex.ru>
2025-05-18 20:09:17 +0400

    fix review

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,21 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
     
     @IBOutlet weak var countTitle: UILabel!
+    @IBOutlet weak var history: UITextView!
+    
+    // MARK: - Properties
+
     private let baseCountTitleText = "Значение счётчика: "
     private var count = 0;
-    let dateFormatter = DateFormatter()
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ru_RU")
+        formatter.dateFormat = "d MMM HH:mm:ss"
+        return formatter
+    }()
     
-    @IBAction func plusButtonDidTap(_ sender: Any) {
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateLabel()
+        history.text = "История изменений:\n"
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction private func plusButtonTapped(_ sender: Any) {
         count += 1
         let now = Date()
         let date = dateFormatter.string(from: now)
         history.text += "[\(date)]: значение изменено на +1\n"
         updateLabel()
+        autoScrollHistory()
     }
-    @IBAction func minusButtonDidTap(_ sender: Any) {
+    
+    @IBAction private func minusButtonTapped(_ sender: Any) {
         let now = Date()
         let date = dateFormatter.string(from: now)
         
@@ -32,32 +55,30 @@ class ViewController: UIViewController {
         } else {
             history.text += "[\(date)]: попытка уменьшить значение счётчика ниже 0\n"
         }
-        
+        autoScrollHistory()
     }
     
-    @IBAction func resetButtonDidTap(_ sender: Any) {
+    @IBAction private func resetButtonTapped(_ sender: Any) {
         count = 0
         let now = Date()
         let date = dateFormatter.string(from: now)
 
         history.text += "[\(date)]: значение сброшено\n"
         updateLabel()
+        autoScrollHistory()
     }
     
-    @IBOutlet weak var history: UITextView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        updateLabel()
-        history.text = "История изменений:\n"
-        dateFormatter.locale = Locale(identifier: "ru_RU")
-        dateFormatter.dateFormat = "d MMM HH:mm:ss"
-    }
+    // MARK: - Private Methods
     
     private func updateLabel() {
         countTitle.text = "\(baseCountTitleText)\(count)"
     }
-
+    
+    
+    private func autoScrollHistory() {
+        let range = NSRange(location: history.text.count - 1, length: 1)
+        history.scrollRangeToVisible(range)
+    }
 
 }
 
